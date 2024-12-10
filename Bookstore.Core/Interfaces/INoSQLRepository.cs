46738286f430c01fdbe5717a285bdccddbd5fc4b
Oliver <Oliverbundgaard@live.dk>
2024-12-10 10:56:48 +0100

    Fixed INoSQLRepository Async

--- Bookstore.Core/Interfaces/INoSQLRepository.cs
+++ Bookstore.Core/Interfaces/INoSQLRepository.cs
@@ -4,9 +4,9 @@ namespace Bookstore.Core.Interfaces;
 
 public interface INoSQLRepository<T>
 {
-    Task Post(T entry);
-    Task Get(string key);
+    Task<T> Post(T entry);
+    Task<T> Get(string key);
     Task<IEnumerable<T>> GetAll();
-    Task Put(T entry);
+    Task<T> Put(T entry);
     Task Delete(string key);
 }
